10b9b67524af06a7993a85478220380fc4f2ec94
gakyol <gabriel.akyol@mhp.com>
2024-04-11 19:36:22 +0200

    added provider

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+    version = "~> 5.44.0"  // Optional: Spezifiziere eine Version des Providers.
+    region  = "eu-central-1"  // Ersetze dies durch die Region, in der du den Bucket erstellen möchtest.
+    }
