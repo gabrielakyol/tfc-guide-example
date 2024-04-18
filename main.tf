8574efe61f8c3c14385c1e9481505c379e8b3cb8
gakyol <gabriel.akyol@mhp.com>
2024-04-18 11:43:55 +0200

    added acls

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "tfe-example"
+resource "aws_s3_bucket" "tfe-example" {
+  bucket = "tfe-example-bucket"
     tags = {
       Name        = "MyS3Bucket"
     }
 }
-resource "aws_s3_bucket_acl" "example" {
+resource "aws_s3_bucket_acl" "tfe-example" {
   bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
