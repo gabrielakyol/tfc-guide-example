30ef029b76cf3f42946a7074b0579d20ac65713a
gakyol <gabriel.akyol@mhp.com>
2024-04-18 11:49:17 +0200

    removed acls

--- main.tf
+++ main.tf
@@ -4,7 +4,3 @@ resource "aws_s3_bucket" "tfe-example" {
       Name        = "MyS3Bucket"
     }
 }
-resource "aws_s3_bucket_acl" "tfe-example" {
-  bucket = aws_s3_bucket.tfe-example.id
-  acl    = "private"
-}
