93288e9e671a8550104e0052ae2d937f07c9d9e3
gakyol <gabriel.akyol@mhp.com>
2024-04-18 11:35:13 +0200

    fix

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "test123" {
   bucket = "test-tfe"
+  acl    = "private"  # Set a valid initial ACL type if necessary
 }
 
 resource "aws_s3_bucket_acl" "mein_bucket_acl" {
-  bucket = aws_s3_bucket.test123.id
-  acl    = "private"
+  bucket = aws_s3_bucket.test123.id # Correctly reference the bucket ID
+  acl    = "private"  # Specify the desired ACL type
 }
