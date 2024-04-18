6b17c4f8cc7d0e3163d2b4395f9d48787bc6306d
gakyol <gabriel.akyol@mhp.com>
2024-04-18 11:25:29 +0200

    added acl

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "aws_s3_bucket" "test123" {
 
   bucket = "test-tfe"
 
-  acl    = "private"
+  acl    = "mein_bucket_acl"
 
+}
+
+resource "aws_s3_bucket_acl" "mein_bucket_acl" {
+  bucket = aws_s3_bucket.test123.id
+  acl    = "private"
 }
