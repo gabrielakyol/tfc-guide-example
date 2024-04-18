cee6019d3939ccb24c482fa96b1b9534d527ef95
gakyol <gabriel.akyol@mhp.com>
2024-04-18 11:17:53 +0200

    123

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_url" {
   description = "Der öffentliche URL des S3 Buckets"
-  value       = "https://${aws_s3_bucket.mein_bucket.bucket}.s3.amazonaws.com/"
+  value       = "https://${aws_s3_bucket.test123.bucket}.s3.amazonaws.com/"
 }
