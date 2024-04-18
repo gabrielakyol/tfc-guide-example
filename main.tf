d70ca479136914fbfbc56885618b5b9b2d295e35
gakyol <gabriel.akyol@mhp.com>
2024-04-18 11:38:45 +0200

    some changes

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
-resource "aws_s3_bucket" "test123" {
-  bucket = "test-tfe"
-  acl    = "private"  # Set a valid initial ACL type if necessary
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-example-bucket"
+    tags = {
+      Name        = "MyS3Bucket"
+      Environment = "Production"
+    }
 }
-
-resource "aws_s3_bucket_acl" "mein_bucket_acl" {
-  bucket = aws_s3_bucket.test123.id # Correctly reference the bucket ID
-  acl    = "private"  # Specify the desired ACL type
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
 }
